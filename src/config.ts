b6a84141a54e57455805315a8a38c6859fc7f8b9
Robert Russell <russellrobert1391@gmail.com>
2025-08-06 00:00:01 -0700

    fixed cors again

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
   SERVER_URL: import.meta.env.PROD 
-    ? 'https://aideath-production.up.railway.app/' // Will update this after Railway deployment
+    ? 'https://aideath-production.up.railway.app' // Will update this after Railway deployment
     : 'http://localhost:3001'
 };
 
